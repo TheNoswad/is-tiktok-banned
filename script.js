f09bdf4f9285f1aafbf2038b762a87008c1425d2
TheNoswad <noswad@noswad.org>
2023-03-28 11:05:00 -0500

    Fix mobile interaction and coloring

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const map = L.map('map').setView([37.8, -96], 4);
 
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+	// const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	// 	maxZoom: 19,
+	// 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	// }).addTo(map);
 
 	// control that shows state info on hover
 	const info = L.control();
@@ -15,22 +15,30 @@ const map = L.map('map').setView([37.8, -96], 4);
 	};
 
 	info.update = function (props) {
-		const contents = props ? `<b>${props.name}</b><br />${props.density} people / mi<sup>2</sup>` : 'Hover over a state';
-		this._div.innerHTML = `<h4>US Population Density</h4>${contents}`;
+		let contents = 'TikTok is Not Banned in the Ocean';
+		if (props != null) {
+			if (props.tiktok_banned == "banned") {
+				contents = `<b>${props.name}</b><br />TikTok is Banned in ` + props.name;
+			}
+			else if (props.tiktok_banned == "government_devices") {
+				contents = `<b>${props.name}</b><br />TikTok is Banned on Government Devices in ` + props.name;
+			}
+			else {
+				contents = `<b>${props.name}</b><br />TikTok is Not Banned in ` + props.name;
+			}
+			// contents = `<b>${props.name}</b><br />${props.tiktok_banned} people / mi<sup>2</sup>`;
+		}
+		// const contents = props ? `<b>${props.name}</b><br />${props.tiktok_banned} people / mi<sup>2</sup>` : 'Hover over a Country';
+		this._div.innerHTML = `<h4>Where is TikTok Banned?</h4>${contents}`;
 	};
 
 	info.addTo(map);
 
 
-	// get color depending on population density value
+	// get color depending on the state of tiktok in that country
 	function getColor(d) {
-		return d > 1000 ? '#800026' :
-			d > 500  ? '#BD0026' :
-			d > 200  ? '#E31A1C' :
-			d > 100  ? '#FC4E2A' :
-			d > 50   ? '#FD8D3C' :
-			d > 20   ? '#FEB24C' :
-			d > 10   ? '#FED976' : '#FFEDA0';
+		return d == "banned" ? '#FF0000' :
+			d == "government_devices" ? '#FFFF00' : '#008000'; // if banned on gov devices, yellow else green
 	}
 
 	function style(feature) {
@@ -40,7 +48,7 @@ const map = L.map('map').setView([37.8, -96], 4);
 			color: 'white',
 			dashArray: '3',
 			fillOpacity: 0.7,
-			fillColor: getColor(feature.properties.density)
+			fillColor: getColor(feature.properties.tiktok_banned)
 		};
 	}
 
@@ -72,17 +80,19 @@ const map = L.map('map').setView([37.8, -96], 4);
 
 	function zoomToFeature(e) {
 		map.fitBounds(e.target.getBounds());
+		highlightFeature(e);
 	}
 
 	function onEachFeature(feature, layer) {
 		layer.on({
 			mouseover: highlightFeature,
 			mouseout: resetHighlight,
-			click: zoomToFeature
+			click: zoomToFeature,
+			touchend: zoomToFeature
 		});
 	}
 
-	map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+	// map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
 
 	const legend = L.control({position: 'bottomright'});
@@ -90,16 +100,22 @@ const map = L.map('map').setView([37.8, -96], 4);
 	legend.onAdd = function (map) {
 
 		const div = L.DomUtil.create('div', 'info legend');
-		const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
+		const grades = ["banned", "government_devices", "not_banned"];
 		const labels = [];
-		let from, to;
 
-		for (let i = 0; i < grades.length; i++) {
-			from = grades[i];
-			to = grades[i + 1];
+		labels.push(` <div class="legenditm red">Banned</div>`);
+		labels.push(` <div class="legenditm yellow">Banned on Government Devices</div>`);
+		labels.push(` <div class="legenditm green">Not Banned</div>`);
+		// const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
+		// const labels = [];
+		// let from, to;
 
-			labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
-		}
+		// for (let i = 0; i < grades.length; i++) {
+		// 	from = grades[i];
+		// 	to = grades[i + 1];
+
+		// 	labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+		// }
 
 		div.innerHTML = labels.join('<br>');
 		return div;
